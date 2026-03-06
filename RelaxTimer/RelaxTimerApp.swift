a9cfe6a6005e1402845c86b3673abb3b523e6a3f
yongmingfan <fanym0919@gmail.com>
2026-03-06 15:39:24 -0500

    Initial Commit

---
+++ RelaxTimer/RelaxTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  RelaxTimerApp.swift
+//  RelaxTimer
+//
+//  Created by Yongming Fan on 3/6/26.
+//
+
+import SwiftUI
+
+@main
+struct RelaxTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
